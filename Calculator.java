a68076296dae2e2d152044b295475628aa45bf30
ideal402 <ideal402@naver.com>
2025-11-17 15:31:36 +0900

    update 25.11.17

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    int sum(int a, int b){
+        return a+b;
+    }
+    int minus(int a, int b){
+        return a-b;
+    }
+    int multiple(int a, int b){
+        return a*b;
+    }
+    int mod(int a, int b){
+        return a/b;
+    }
+    public static void main(String[] args){
+
+    }
+}
